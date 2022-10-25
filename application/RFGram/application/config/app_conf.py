a047af3512a96b54ac1bde9226645584e22b7c35
Kobanoss <mlgspiritofficial@gmail.com>
2022-10-25 07:02:41 +0300

    Modified app conf - fix db URL

--- application/RFGram/application/config/app_conf.py
+++ application/RFGram/application/config/app_conf.py
@@ -1,8 +1,10 @@
 from application.config.db_conf import DB_CONF
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class APP_CONF:
     PARAMETERS = {'SECRET_KEY': '0eeff27f9c89ab975e01c1eb5aeef5148b1810e9b690a77bedd94a261b3d9b98',
-                  'SQLALCHEMY_DATABASE_URI': f'sqlite:///{DB_CONF.DB_FILENAME}',
+                  'SQLALCHEMY_DATABASE_URI': f'{os.getenv("DATABASE_URL", "sqlite://")}',
                   'SQLALCHEMY_TRACK_MODIFICATIONS': True,
                   'FLASK_ADMIN_SWATCH': 'cerulean'}
