3ebcded2a158fde3e28ca0f04b1e6a8054a2b5c0
Kobanoss <mlgspiritofficial@gmail.com>
2022-10-25 07:07:36 +0300

    Added some syntax fix [flake8]

--- application/RFGram/application/config/app_conf.py
+++ application/RFGram/application/config/app_conf.py
@@ -3,6 +3,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class APP_CONF:
     PARAMETERS = {'SECRET_KEY': '0eeff27f9c89ab975e01c1eb5aeef5148b1810e9b690a77bedd94a261b3d9b98',
                   'SQLALCHEMY_DATABASE_URI': f'{os.getenv("DATABASE_URL", "sqlite://")}',
